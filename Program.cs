dc2af7c8e7413c884cddb2fc62a70bad471ba84f
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-13 15:31:44 -0300

    Initial test for Person

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿Console.WriteLine("Hello, World!");
+
+string firstname = "Vinícius";
+string lastname = "Oliveira";
+string nickname = "vini";
+string email = "vini@vini.com";
+
+Person p = new(firstname, lastname, nickname, email);
+
+Console.WriteLine(p.ToDict());
+
+Dictionary<string, string> personDict = p.ToDict();
+
+Console.WriteLine(personDict.Keys);
+
+foreach (string key in personDict.Keys)
+    Console.WriteLine($"{key}: {personDict[key]}");
