d505fff8ce52958998c830755b1f6dade6e8272a
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-14 22:26:09 -0300

    Some notes about List

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ string lastname = "Oliveira";
 string nickname = "vini";
 string email = "vini@vini.com";
 
-Person p = new(firstname, lastname, nickname, email);
+Person person = new(firstname, lastname, nickname, email);
 
 Console.WriteLine(p.ToDict());
 
@@ -15,3 +15,46 @@ Console.WriteLine(personDict.Keys);
 
 foreach (string key in personDict.Keys)
     Console.WriteLine($"{key}: {personDict[key]}");
+
+//params
+
+//ArrayList - não é fortemente tipada.
+//Add, Insert(index, elem), AddRange - ArrayList()
+//RemoveAt, Remove, RemoveRange - ArrayList()
+// Contains(elem) - ArrayList()
+// arrayList.Sort() -- quick sort.
+// Array.Sort(array)
+// ArrayList não oferece o melhor desempenho, recomenda-se utilizar sempre List!
+
+/*
+
+List<T>
+
+é fortemente tipada.
+
+Add(), AddRange(), Insert(), InsertRange()
+Remove(), RemoveAt(), RemoveRange(),
+Contains(), 
+Count
+Sort(), Clear()
+
+Find(), FindLast(), FindIndex(), FindLastIndex(), FindAll()
+
+Predicado: é uma função, mas pode ser representado também pela expressão lambda: x => x * x
+
+Box e unboxing atrapalham o desempenho.
+
+Avaliar uso de IEnumerable(lazy evaluation + performance em nível de memória) e List.
+
+Métodos de extensão
+
+Any(), OrderBy(), 
+
+ */
+
+List<Person> list = new();
+list.Add(person);
+
+List<string> frutas = new() { "Banana", "Maça", "Laranja" };
+
+frutas.Find(fruta => fruta.Contains("B"));
